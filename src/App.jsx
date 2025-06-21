3556aeef97590157942e76f34545f89b641a2bc2
Shubham Yadav <shubhamyadavms@gmail.com>
2025-06-21 19:31:32 +0530

    props example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import PhotoGallery from "./PhotoGallery";
+const ParentComponent = () => {
+  return (
+    <div>
+      <ChildComponent name="shuhb" age={28} />
+      <ChildComponent name="raj" age={18} />
+    </div>
+  );
+};
+
+const ChildComponent = (props) => {
+  const { name, age } = props;
+  return (
+    <div>
+      <h1>{name + " " + age}</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      <PhotoGallery />
+      <ParentComponent />
     </div>
   );
 };
