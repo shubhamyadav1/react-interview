a736ddeda004dffa1021d9b0e1db6ecc02c9ac43
Shubham Yadav <shubhamyadavms@gmail.com>
2025-06-22 19:50:06 +0530

    Counter Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 
-const ParentComponent = () => {
-  const [message, setMessage] = useState("");
-  const handleDataFromChild = (childData) => {
-    setMessage(childData);
-  };
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>Parent Component</h1>
-      <p>Message from child: {message}</p>
-      <ChildComponent sendDataToParent={handleDataFromChild} />
-    </div>
-  );
-};
-
-const ChildComponent = (props) => {
-  const { sendDataToParent } = props;
-  const handleClick = () => {
-    sendDataToParent("Hello Parents");
-  };
-  return (
-    <div>
-      <h1>Child Component</h1>
-      <button onClick={handleClick}>Send Message to Parent</button>
+      <p>count: {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={function () {
+          return setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Clear
+      </button>
     </div>
   );
 };
@@ -31,7 +34,7 @@ const ChildComponent = (props) => {
 const App = () => {
   return (
     <div>
-      <ParentComponent />
+      <Counter />
     </div>
   );
 };
