28aaf0d2df4de5bb69897a76af85fa9fb0c96883
Shubham Yadav <shubhamyadavms@gmail.com>
2025-06-22 21:21:09 +0530

    children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from "react";
+// children props are special props that lets you pass JSX inside a component.
+// children props example
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
+const Card = ({ children }) => {
   return (
-    <div>
-      <p>count: {count}</p>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        onClick={function () {
-          return setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        Clear
-      </button>
+    <div
+      style={{
+        border: "2px solid black",
+        padding: "16px",
+        borderRadius: "8px",
+      }}
+    >
+      {children}
     </div>
   );
 };
@@ -34,9 +18,16 @@ const Counter = () => {
 const App = () => {
   return (
     <div>
-      <Counter />
+      <Card>
+        <h2>User Profile</h2>
+        <p>Name: Alice</p>
+      </Card>
+
+      <Card>
+        <h2>Settings</h2>
+        <p>Dark Mode: Enabled</p>
+      </Card>
     </div>
   );
 };
-
 export default App;
